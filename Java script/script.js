d3272d354d6ee7035035887b924f5dd772e9b3b1
UNFlappable <harunyoney@gmail.com>
2024-02-17 11:13:15 +0300

    js practise

--- Java script/script.js
+++ Java script/script.js
@@ -1,53 +1,75 @@
 let products = [
-    { name: "Product 1", price: 20, category: "Electronics" },
-    { name: "Product 2", price: 30, category: "Clothes" },
-    { name: "Product 3", price: 40, category: "Electronics" },
-    { name: "Product 4", price: 50, category: "Clothes" },
-    { name: "Product 5", price: 60, category: "Clothes" },
-    { name: "Product 6", price: 70, category: "Electronics" },
-    { name: "Product 7", price: 80, category: "Clothes" },
-    { name: "Product 8", price: 90, category: "Electronics" },
+  { name: "Product 1", price: 20, category: "Electronics" },
+  { name: "Product 2", price: 30, category: "Clothes" },
+  { name: "Product 3", price: 40, category: "Electronics" },
+  { name: "Product 4", price: 80, category: "Clothes" },
+  { name: "Product 5", price: 60, category: "Clothes" },
+  { name: "Product 6", price: 70, category: "Electronics" },
+  { name: "Product 7", price: 80, category: "Clothes" },
+  { name: "Product 8", price: 90, category: "Electronics" },
+  { name: "Product 8", price: 40, category: "Electro" },
+  { name: "Product 8", price: 70, category: "Elecxtro" },
+  { name: "Product 8", price: 80, category: "Elecxtro" },
 ];
+const hesaplama = (array) => {
+
+//! aynı  categoryleri çeşitlerini tek yazma.
+  const categories = [];
+
+    array.forEach((product) => {
+    if (!categories.includes(product.category)) {
+      categories.push(product.category);
+    }
+  });
+
+
+  let sonucc = [];
+  for (i = 0; i < categories.length; i++) {
+    const above50 = array
+      .filter((p) => p.category === categories[i])
+      .map((k) => k.price);
+
+    const avarage = above50.reduce((a, b) => a + b) / above50.length;
+    if (avarage >= 50) {
+      sonucc.push(`category:${categories[i]}-avarage:${avarage}`);
+    }
+  }
+  return console.log(sonucc);
+};
+hesaplama(products);
+
+
+
+const employees = [
+  { name: "John", salary: 50000, department: "IT" },
+  { name: "Jane", salary: 60000, department: "HR" },
+  { name: "Bob", salary: 55000, department: "IT" },
+  { name: "Sophie", salary: 75000, department: "HR" },
+  { name: "Mike", salary: 65000, department: "IT" },
+  { name: "Emily", salary: 80000, department: "HR" },
+  { name: "David", salary: 70000, department: "IT" },
+];
+
 // console.log(products[products.length-1].category);
 
+// const above50 = products.filter((p) => (p.category === "Electronics")).map( k=>k.price)
 
+// const avarage50 = above50.reduce((a,b)=>(a+b))/above50.length
 
-
-const above50 = products.filter((p) => (p.category === "Electronics")).map( k=>k.price)
-
-
-
-const avarage50 = above50.reduce((a,b)=>(a+b))/above50.length
-
-console.log(` category: 'Electronics'average: ${avarage50}`);
-console.log(above50);
-console.log(typeof(above50));
-
-console.log(products);
-
+// console.log(` category: 'Electronics'average: ${avarage50}`);
+// console.log(above50);
+// console.log(typeof(above50));
 
 //    You are given an array of objects representing a collection of products, each with a name, price, and category. Your task is to use map, filter, and reduce to calculate the average price of products in each  category, and then return an array of objects containing only the categories that have an average price  above 50.
 
 const Elect = [];
 products.map((catagorys) => {
-    if (products.category === "Electronics") {
-        Elect = catagorys;
-    }
+  if (products.category === "Electronics") {
+    Elect = catagorys;
+  }
 });
 
-console.log(Elect);
-
-
-
-const employees = [
-    { name: "John", salary: 50000, department: "IT" },
-    { name: "Jane", salary: 60000, department: "HR" },
-    { name: "Bob", salary: 55000, department: "IT" },
-    { name: "Sophie", salary: 75000, department: "HR" },
-    { name: "Mike", salary: 65000, department: "IT" },
-    { name: "Emily", salary: 80000, department: "HR" },
-    { name: "David", salary: 70000, department: "IT" },
-];
+// console.log(Elect);
 
 [{ department: "HR", average: 71666 }];
 
@@ -55,24 +77,23 @@ const employees = [
 
 //! her bir department için bu kodu döndürsün ve aldığın ortalama değeri her bir objenin sonuna push yapsın.
 
-const salaries = employees.filter(depart=>depart.department==="IT").map(i=>i.salary)
-const average11 = salaries.reduce(( a,b )=> a+b ,0 )/salaries.length
+const salaries = employees
+  .filter((depart) => depart.department === "IT")
+  .map((i) => i.salary);
+const average11 = salaries.reduce((a, b) => a + b, 0) / salaries.length;
 
-if (average11>=65000) {console.log("ajsdn");}
-
-
-
-
+if (average11 >= 65000) {
+  console.log("ajsdn");
+}
 
 // salaries.reduce(( a,b )=> a+b ,0 )
-console.log(average11);
-console.log(salaries);
+// console.log(average11);
+// console.log(salaries);
 
-const h2 =document.querySelector("h2")
-const input =document.querySelector("input")
+// const h2 =document.querySelector("h2")
+// const input =document.querySelector("input")
 
-document.addEventListener("keydown",run=>console.log(run.key))
-document.querySelector("input").addEventListener("onclick",run=>console.log(run.value))
+// document.addEventListener("keydown",run=>console.log(run.key))
+// document.querySelector("input").addEventListener("onclick",run=>console.log(run.value))
 
-
-console.log(h2.textContent);
+// console.log(h2.textContent);
--- Java script/script3.js
+++ Java script/script3.js
@@ -1,11 +1,25 @@
-let isim = "harun"
-let soyisim="yny"
-
-console.log(soyisim);
 
 
 // 1. Soru: Bir kullanıcının adını ve soyadını alıp, bunları "Soyadı, Adı" formatında birleştirmek istiyorsunuz. Ancak, ad ve soyad başlangıçta küçük harflerle girilmiş olabilir. İlk harfleri büyük harf yaparak bu formatlamayı nasıl gerçekleştirirsiniz?
 // isimFormatla(ad, soyad); // "Yılmaz, Ahmet"
+
+
+let isim = "harun"
+let soyisim="yny"
+
+
+const isimFormatla = (isim,soyisim)=>{
+    let y =isim[0].toLocaleUpperCase() + isim.slice(1)
+let x =soyisim[0].toLocaleUpperCase() + soyisim.slice(1)
+
+return `${y} ${x} `
+}
+
+console.log(isimFormatla(soyisim,isim))
+
+
+
+
 
 
 
@@ -14,12 +28,36 @@ console.log(soyisim);
 // input :"Pijamalı hasta, yağız şoföre çabucak güvendi.";
 // output : // Pijamali hasta, yagiz sofore cabucak guvendi.
 
+const text = "Pijamalı hasta, yağız şoföre çabucak güvendi."
+
+const replaceToEng = (text) => {text.replace("ı","i")}
+console.log(replaceToEng(text));
+
+
+
+
+
 
 
 // 3. Soru: İki sayı dizisi içindeki ortak elemanları bulup yeni bir dizi oluşturun?
 // input : [1, 2, 3, 4, 5], [3, 4, 5, 6, 7]
 // output : [3, 4, 5]
 
+
+
+
+const sayilar1 =[1, 2, 3, 4, 5]
+const sayilar2 =[3, 4, 5, 6, 7]
+
+
+for(let i=0;i<=sayilar1.length;i++){
+
+    for(let j=0;j<=sayilar2.length;j++){
+//   console.log(j);
+//     console.log("***");
+    }
+  
+}
 
 
 // 4. Soru: Soru: Bir dizideki sayıların karelerinden çift sayıların iki katlarının toplamını bulun.
